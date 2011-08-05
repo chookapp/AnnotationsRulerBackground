3bd3e75d30235add5599c2fa61ea473f5a5561ee
Gil Barash <chookapp@yahoo.com>
2011-08-05 20:33:24 +0200

    Several cosmetic issues...

--- OverviewRulerBackground/src/com/chookapp/org/overviewrulerbackground/core/EditorEvents.java
+++ OverviewRulerBackground/src/com/chookapp/org/overviewrulerbackground/core/EditorEvents.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) Gil Barash - chookapp@yahoo.com
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Gil Barash - initial API and implementation
+ *    
+ * Thanks to:
+ *    emil.crumhorn@gmail.com - Some of the code was coped from the 
+ *    "eclipsemissingfeatrues" plugin. 
+ *******************************************************************************/
+
+
 package com.chookapp.org.overviewrulerbackground.core;
 
 import java.lang.reflect.Field;
